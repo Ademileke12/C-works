1cb0df909dfa38651ebd2a55685f13a790971513
Ademileke12 <ademilekeabudu@gmail.com>
2022-07-26 14:45:50 +0100

    c-works_calculatorof2

---
+++ calculator_of_2numbers_with_a_menu.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+void operators()
+{
+	cout<<"1.addition"<<endl;
+	cout<<"2.division"<<endl;
+	cout<<"3.multiplication"<<endl;
+	cout<<"4.subtraction"<<endl;
+	cout<<"select one of the options above"<<endl;
+	
+	float add , sub , divi , multi , valA, valB, total, optionval;
+	cin>> optionval;
+	if(optionval == 1){
+	cout<<"type in values of a and b"<<endl;
+	cout<<"a:";
+	cin>>valA;
+	cout<<"b:";
+	cin>>valB;
+	total = valA + valB;
+	cout<<"the sum of "<<valA<<" and "<<valB<<" is:"<<total<<endl;
+  }
+
+    if(optionval == 2){
+	cout<<"type in values of a and b"<<endl;
+	cout<<"a:";
+	cin>>valA;
+	cout<<"b:";
+	cin>>valB;
+	total = valA / valB;
+	cout<<"the division of "<<valA<<" and "<<valB<<" is:"<<total<<endl;
+    }
+    
+    if(optionval == 3){
+	cout<<"type in values of a and b"<<endl;
+	cout<<"a:";
+	cin>>valA;
+	cout<<"b:";
+	cin>>valB;
+	total = valA * valB;
+	cout<<"the multiplication of "<<valA<<" and "<<valB<<" is:"<<total<<endl;
+    }
+    
+    if(optionval == 4){
+	cout<<"type in values of a and b"<<endl;
+	cout<<"a:";
+	cin>>valA;
+	cout<<"b:";
+	cin>>valB;
+	total = valA - valB;
+	cout<<"the subtract of "<<valA<<" and "<<valB<<" is:"<<total<<endl;
+    }
+
+	cout<<"Type in 1 to Return to menu and 2 to exit calculator"<<endl;
+	int i;
+	cin>> i;
+	while (i == 1)
+	{
+		i++;
+		operators();
+	}
+
+	if (i > 1 || i < 1)
+	{
+		exit(0);
+	}
+}
+
+int main(){
+	operators();
+	return 0;
+}
